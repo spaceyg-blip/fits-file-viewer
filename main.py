8d451acef7739962ed6abccccc45d25e21b57d41
spaceyg-blip <tech.com.g07@gmail.com>
2026-05-19 14:24:16 +0530

    Connect FITS modules to main script

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("FITS File Viewer Started")
+from src.load_fits import load_fits
+from src.show_header import show_header
+from src.plot_image import plot_image
+
+file_path = "data/sample.fits"
+
+data, header = load_fits(file_path)
+
+show_header(header)
+
+plot_image(data)
